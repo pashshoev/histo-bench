5fffd52f6d3712220b4ee3289f345a4533823fbe
Bakhtierzhon Pashshoev <bakhtierzhon.pashshoev@check24.de>
2025-08-06 13:21:55 +0200

    fix gdc installation script

--- bash_commands/install_gdc_ubuntu.sh
+++ bash_commands/install_gdc_ubuntu.sh
@@ -1,4 +1,9 @@
+sudo apt-get install unzip -y
+
 wget https://gdc.cancer.gov/system/files/public/file/gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
 unzip gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
-sudo mv gdc-client /usr/local/bin/
+unzip gdc-client_2.3_Ubuntu_x64.zip
+sudo mv gdc-client /usr/local/bin
 rm gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
+rm gdc-client_2.3_Ubuntu_x64.zip
+rm gdc-client
