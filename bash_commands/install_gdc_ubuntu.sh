aa8a4af2c425f3a8505976124575d7552d83db05
Bakhtierzhon Pashshoev <bakhtierzhon.pashshoev@check24.de>
2025-08-06 13:02:33 +0200

    add gdc installation script

---
+++ bash_commands/install_gdc_ubuntu.sh
@@ -0,0 +1,4 @@
+wget https://gdc.cancer.gov/system/files/public/file/gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
+unzip gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
+sudo mv gdc-client /usr/local/bin/
+rm gdc-client_2.3_Ubuntu_x64-py3.8-ubuntu-20.04.zip
